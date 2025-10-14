edd90815ba729f54233267b666b84dda2cc176e1
Ladislav Spevák <ladislav.spevak@gmail.com>
2025-10-15 01:37:57 +0200

    Added Type , and handle_name_edit, formatting

--- server/config.py
+++ server/config.py
@@ -1,21 +1,30 @@
 import os
 import json
 
-from utils import GRAY, RESET
+from utils import GREEN, GRAY, RESET
 path = os.path.join(os.path.dirname(__file__), "config.json")
 
 with open(path, "r", encoding="utf-8") as f:
     config = json.load(f)
 ID = config["ID"]
 NAME = config["NAME"]
+TYPE = config["TYPE"]
 VERSION = config["VERSION"]
 HOST = config["HOST"]
 PORT = config["PORT"]
 
-def handle_name(name):
+def handle_name_edit(name):
     global NAME
+    FROM = NAME
     NAME = name
     config["NAME"] = name
     with open(path, "w", encoding="utf-8") as f:
         json.dump(config, f, indent=4)
-    print(f"{GRAY}@{RESET} name change to {name} in config.json")
+    gap = 30
+    print("\n" + " " * 5 + f"{GREEN}{NAME}#{ID}{RESET} had been changed")
+    print(" " * 5 + f"{GRAY}{'─' * 50}{RESET}")
+    print(" " * 5 + f"{'Type:'.ljust(gap)} {TYPE}")
+    print(" " * 5 + f"{'From:'.ljust(gap)} {GRAY}{FROM}{RESET}")
+    print(" " * 5 + f"{'To:'.ljust(gap)} {GREEN}{NAME}{RESET}")
+    print("\n" +" " * 5 + "To check changes, open config.json")
+    FROM = None
