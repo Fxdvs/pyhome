000e836c523d13feb6c0bef55f921879097cb86a
Ladislav Spevák <ladislav.spevak@gmail.com>
2025-10-15 01:37:28 +0200

    Added commands

--- utils/commands.py
+++ utils/commands.py
@@ -1,13 +1,15 @@
 commands = [
     {'name': 'clear, cls', 'description': 'Clears the console', 'type': 'client/server'},
     {'name': 'clear list, clear ls', 'description': 'Clears the client list', 'type': 'server'},
+    {'name': 'config/conf load', 'description': 'Loads config from config.json', 'type': 'server'},
     {'name': 'disconnect, dc', 'description': 'Disconnect from server.', 'type': 'client'},
     {'name': 'exit, quit', 'description': 'Exits the application', 'type': 'client/server'},
     {'name': 'help, commands, ?', 'description': 'Show this help', 'type': 'client/server'},
-    {'name': 'info, self, about', 'description': 'Information about the application', 'type': 'client/server'},
+    {'name': 'self, about', 'description': 'Information about the application', 'type': 'client/server'},
     {'name': 'list, ls', 'description': 'List all connected clients', 'type': 'server'},
-    {'name': 'name', 'description': 'Changes name of the application.', 'type': 'client/server'},
+    {'name': 'name edit', 'description': 'Changes name of the application.', 'type': 'client/server'},
     {'name': 'reconnect', 'description': 'Reconnect to server.', 'type': 'client'},
     {'name': 'restart', 'description': 'Restarts the application.', 'type': 'client/server'},
     {'name': 'send <message> (host:port@name)', 'description': 'Sends message to host:port@name', 'type': 'client/server'},
 ]
+
