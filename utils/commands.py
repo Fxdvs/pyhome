06cdf84438d16e9730aa15394768c49fdd16f8d5
Ladislav Spevák <ladislav.spevak@gmail.com>
2025-10-15 02:45:20 +0200

    Removed config load

--- utils/commands.py
+++ utils/commands.py
@@ -1,11 +1,11 @@
 commands = [
     {'name': 'clear, cls', 'description': 'Clears the console', 'type': 'client/server'},
     {'name': 'clear list, clear ls', 'description': 'Clears the client list', 'type': 'server'},
-    {'name': 'config/conf load', 'description': 'Loads config from config.json', 'type': 'server'},
     {'name': 'disconnect, dc', 'description': 'Disconnect from server.', 'type': 'client'},
     {'name': 'exit, quit', 'description': 'Exits the application', 'type': 'client/server'},
     {'name': 'help, commands, ?', 'description': 'Show this help', 'type': 'client/server'},
     {'name': 'self, about', 'description': 'Information about the application', 'type': 'client/server'},
+    {'name': 'show config, show conf', 'description': 'Shows formatted config.json', 'type': 'server'},
     {'name': 'list, ls', 'description': 'List all connected clients', 'type': 'server'},
     {'name': 'name edit', 'description': 'Changes name of the application.', 'type': 'client/server'},
     {'name': 'reconnect', 'description': 'Reconnect to server.', 'type': 'client'},
